4e14d8f48069195d5ffaf6f947a956f18d571680
i_am_tatha <57251093+iamtatha@users.noreply.github.com>
2020-05-23 03:55:13 +0530

    Add files via upload

---
+++ Feedback_Form/js.js
@@ -0,0 +1,9 @@
+function Caution(){
+	var x=document.getElementById('caution');
+	x.style.display='inline-block';
+}
+
+function Hide(){
+	var x=document.getElementById('caution');
+	x.style.display='none';
+}
